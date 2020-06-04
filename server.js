ff370a111701a503f45b86ec463a0b4583046713
Vishal <vishalbhola5@gmail.com>
2020-06-04 18:49:45 +0530

    Dependencies & Back-end Routes

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.json({msg: "Welcome to Contact Kepper"});
+})
+
+
+// Define Routes
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on Port ${PORT}`)
+});
