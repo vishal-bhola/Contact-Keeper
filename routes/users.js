bf06a010354368abc8c5ebce4f1920cfbb48c332
Vishal <vishalbhola5@gmail.com>
2020-06-05 17:47:12 +0530

    create JWT

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
@@ -41,7 +43,19 @@ async (req,res) => {
         user.password = await bcrypt.hash(password, salt);
 
         await user.save();
-        res.send("User Saved");
+        
+        const payload = {
+            user:{
+                id: user.id
+            }
+        }
+
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 3600
+        }, (err,token)=> {
+            if(err) throw err;
+            res.json({ token });
+        })
 
     } catch (err) {
         console.error(err.message);
