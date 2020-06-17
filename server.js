03e2fdad20846361891db71d80c094f54ef78f4e
Vishal <vishalbhola5@gmail.com>
2020-06-17 14:27:03 +0530

    Production Deployment Changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = rqeuire('path');
 
 const app = express();
 
@@ -9,17 +10,22 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req,res)=>{
-    res.json({msg: "Welcome to Contact Kepper"});
-})
-
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
+// Server static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+
+}
 
 const PORT = process.env.PORT || 5000;
 
